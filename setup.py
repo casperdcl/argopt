988218f916e39dfb622be6f12518032245c635f3
Casper da Costa-Luis <casper.dcl@physics.org>
2017-05-23 02:45:01 +0100

    fix travis pypy3, add py36

--- setup.py
+++ setup.py
@@ -212,6 +212,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: IronPython',
         'Programming Language :: Python :: Implementation :: PyPy',
